702bab5a5b42921cd44a3ff0d6ba950b6868d47f
davidsusu <horvath@webarticum.hu>
2015-01-25 00:16:16 +0100

    IndexedStore added

---
+++ treepgrahics/src/treegraphics/indexedstore/IndexedStore.java
@@ -0,0 +1,30 @@
+package treegraphics.indexedstore;
+
+import java.util.Comparator;
+import java.util.List;
+
+public interface IndexedStore<T> {
+
+	public void addIndex(String indexName, Comparator<T> comparator);
+	
+	public void removeIndex(String indexName);
+	
+	public void hasIndex(String indexName);
+	
+	public void hasIndex(Comparator<T> comparator);
+	
+	public void addItem(T item);
+	
+	public void removeItem(T item);
+	
+	public boolean hasItem(T item);
+
+	public List<T> getAll();
+
+	public List<T> getAll(String orderIndexName);
+	
+	public List<T> getFiltered(String filterIndexName, T fromItem, T toItem);
+
+	public List<T> getFiltered(String filterIndexName, T fromItem, T toItem, String orderIndexName);
+	
+}
