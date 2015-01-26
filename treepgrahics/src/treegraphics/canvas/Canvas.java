d2c0bdb969fa36e5916942a4b6a28320b3a3b746
horvath <horvath@patronet.net>
2015-01-26 18:30:49 +0100

    Canvas: origin and zoom added (weakly demonstrated)

--- treepgrahics/src/treegraphics/canvas/Canvas.java
+++ treepgrahics/src/treegraphics/canvas/Canvas.java
@@ -2,6 +2,16 @@ package treegraphics.canvas;
 
 public interface Canvas {
 
+	public void setOrigin(Point origin);
+	
+	public Point getOrigin();
+	
+	public void setZoom(double zoom);
+	
+	public double getZoom();
+	
+	public void resetTransform();
+	
 	public void setColor(Color color);
 	
 	public void fillRectangle(Rectangle rectangle);
--- treepgrahics/src/treegraphics/canvas/test/SwingCanvasTest.java
+++ treepgrahics/src/treegraphics/canvas/test/SwingCanvasTest.java
@@ -6,6 +6,9 @@ import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -26,6 +29,9 @@ public class SwingCanvasTest {
 		
 		final RectangleDrawable mouseRectangleDrawable = new RectangleDrawable(new Rectangle(-10, -10, 0, 0), new Color(0, 0, 0));
 
+		final Point origin = new Point(0, 0);
+		final Wrapper<Double> zoom = new Wrapper<Double>(1.0);
+		
 		final Color catcherColor = new Color(0, 0, 170);
 		final Color catcherHighlightColor = new Color(100, 100, 255);
 		final Color mouseColor = new Color(150, 150, 150);
@@ -41,6 +47,10 @@ public class SwingCanvasTest {
 			@Override
 			protected void paintComponent(Graphics g) {
 				AwtCanvas canvas = new AwtCanvas((Graphics2D)g);
+				
+				// FIXME
+				canvas.setOrigin(origin);
+				canvas.setZoom(zoom.getObject());
 				
 				int width = getWidth();
 				int height = getHeight();
@@ -120,6 +130,16 @@ public class SwingCanvasTest {
 				}
 			}
 		});
+		mainPanel.addMouseWheelListener(new MouseWheelListener() {
+			
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent ev) {
+				double diff = ev.getPreciseWheelRotation();
+				zoom.setObject(zoom.getObject()+(diff/10));
+				mainPanel.repaint();
+			}
+			
+		});
 		
 		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
 		frame.pack();
@@ -130,8 +150,47 @@ public class SwingCanvasTest {
 
 		protected Graphics2D g2d;
 		
+		protected Point origin = new Point(0, 0);
+		
+		protected double zoom = 1;
+		
 		public AwtCanvas(Graphics2D g2d) {
 			this.g2d = g2d;
+		}
+
+		@Override
+		public void setOrigin(Point origin) {
+			this.origin = origin;
+			refreshTransform();
+		}
+
+		@Override
+		public Point getOrigin() {
+			return origin;
+		}
+
+		@Override
+		public void setZoom(double zoom) {
+			this.zoom = zoom;
+			refreshTransform();
+		}
+
+		@Override
+		public double getZoom() {
+			return zoom;
+		}
+
+		@Override
+		public void resetTransform() {
+			this.origin = new Point(0, 0);
+			this.zoom = 1;
+			g2d.setTransform(new AffineTransform());
+		}
+		
+		protected void refreshTransform() {
+			g2d.setTransform(new AffineTransform());
+			g2d.translate(origin.getX(), origin.getY());
+			g2d.scale(zoom, zoom);
 		}
 		
 		@Override
@@ -174,6 +233,24 @@ public class SwingCanvasTest {
 		@Override
 		public Rectangle getReservedRectangle() {
 			return rectangle;
+		}
+		
+	}
+	
+	protected static class Wrapper<T> {
+		
+		protected T object;
+		
+		public Wrapper(T object) {
+			this.object = object;
+		}
+
+		public void setObject(T object) {
+			this.object = object;
+		}
+
+		public T getObject() {
+			return object;
 		}
 		
 	}
