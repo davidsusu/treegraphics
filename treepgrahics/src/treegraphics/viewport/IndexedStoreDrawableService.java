a19957206aa6f870e89c36d5035e78ba51130b20
horvath <horvath@patronet.net>
2015-01-29 21:21:24 +0100

    Warning fixed

--- treepgrahics/src/treegraphics/viewport/IndexedStoreDrawableService.java
+++ treepgrahics/src/treegraphics/viewport/IndexedStoreDrawableService.java
@@ -3,7 +3,6 @@ package treegraphics.viewport;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 import treegraphics.canvas.Canvas;
